da29afef2cea5d8346ace81b79cde01fb21987b4
SasikumarRajasekar <pprsasikumar1992@gmail.com>
2022-02-19 20:03:59 +0530

    Updated

--- src/main/java/com/collegeadmission/impl/UserDaoImpl.java
+++ src/main/java/com/collegeadmission/impl/UserDaoImpl.java
@@ -15,14 +15,17 @@ public class UserDaoImpl {
 
 	Scanner sc = new Scanner(System.in);
 
-	public void insert(UserDetails str) throws ClassNotFoundException, SQLException
+	public void insert(UserDetails str) throws ClassNotFoundException, SQLException {
 
-	{
+		Connection con = null;
+		PreparedStatement pstmt = null;
 
-		Connection con = ConnectionUtil.getDBConnect();
+		try {
+		
+		 con = ConnectionUtil.getDBConnect();
 		String query = "insert into user_details(Name,User_Name,Email,Mobile_Number,User_Password) values(?,?,?,?,?)";
 
-		PreparedStatement pstmt = con.prepareStatement(query);
+		pstmt = con.prepareStatement(query);
 
 		pstmt.setString(1, str.getName());
 		pstmt.setString(2, str.getUserName());
@@ -31,37 +34,81 @@ public class UserDaoImpl {
 		pstmt.setString(5, str.getUserPassword());
 
 		pstmt.executeUpdate();
+		
+	}
+
+	catch (Exception e) {
+		e.getMessage();
+	} finally {
+		ConnectionUtil.close(pstmt, con);
+	}
 	}
 
 	public ResultSet getEmailDetails(UserDetails rp) throws SQLException, ClassNotFoundException {
-		Connection con = ConnectionUtil.getDBConnect();
-		String query = "select * from user_details where email=?";
-		PreparedStatement pstmt = con.prepareStatement(query);
-		pstmt.setString(1, rp.getEmail());
-		ResultSet rs = pstmt.executeQuery();
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+
+			con = ConnectionUtil.getDBConnect();
+			String query = "select * from user_details where email=?";
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, rp.getEmail());
+			rs = pstmt.executeQuery();
+
+		} catch (ClassNotFoundException e) {
+
+			e.printStackTrace();
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			ConnectionUtil.close(rs, pstmt, con);
+
+		}
 		return rs;
 	}
 
 	public UserDetails login(String emailId, String Password) throws ClassNotFoundException, SQLException {
 
-		Connection con = ConnectionUtil.getDBConnect();
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		UserDetails userDetails = null;
+
+		try {
+		
+		 con = ConnectionUtil.getDBConnect();
 
 		String query = "select * from user_details where Email= ? and User_Password= ? ";
 
-		PreparedStatement stmt = con.prepareStatement(query);
+		 pstmt = con.prepareStatement(query);
 
 		// System.out.println("Registered");
 
-		stmt.setString(1, emailId);
-		stmt.setString(2, Password);
+		pstmt.setString(1, emailId);
+		pstmt.setString(2, Password);
 
-		ResultSet rs = stmt.executeQuery();
-		UserDetails userDetails = null;
+		rs = pstmt.executeQuery();
+		//UserDetails userDetails = null;
 		if (rs.next()) {
 			userDetails = new UserDetails(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4),
 					rs.getLong(5), rs.getString(6));
-			return userDetails;
+			//return userDetails;
 		}
+		}
+		
+
+	catch (Exception e) {
+		e.getMessage();
+	} finally {
+		ConnectionUtil.close(rs, pstmt, con);
+	}
 
 		return userDetails;
 	}
@@ -69,30 +116,30 @@ public class UserDaoImpl {
 	public List<UserDetails> showAllUsers() throws ClassNotFoundException, SQLException {
 		List<UserDetails> userList = new ArrayList<UserDetails>();
 		String showusersquery = "select * from user_details";
-		Connection con = ConnectionUtil.getDBConnect();
-		PreparedStatement ps;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
 		try {
 			con = ConnectionUtil.getDBConnect();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			ps = con.prepareStatement(showusersquery);
-			ResultSet rs = ps.executeQuery();
+
+			pstmt = con.prepareStatement(showusersquery);
+			rs = pstmt.executeQuery();
 			while (rs.next()) {
 				// System.out.println(rs.getString(2));
 				UserDetails userdetails = new UserDetails(rs.getInt(1), rs.getString(2), rs.getString(3),
 						rs.getString(4), rs.getLong(5), rs.getString(6));
 				userList.add(userdetails);
 			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+		} catch (ClassNotFoundException e) {
+
 			e.printStackTrace();
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		} finally {
+			ConnectionUtil.close(rs, pstmt, con);
 		}
+
 		return userList;
 	}
 
