fd80b6a6b532727d1100040d05fff42b5ab4add5
SasikumarRajasekar <pprsasikumar1992@gmail.com>
2022-02-19 15:25:07 +0530

    Updated

--- src/main/java/com/collegeadmission/impl/ApplicationStatusImpl.java
+++ src/main/java/com/collegeadmission/impl/ApplicationStatusImpl.java
@@ -9,45 +9,67 @@ import java.util.List;
 
 import com.collegeadmission.connection.ConnectionUtil;
 import com.collegeadmission.model.ApplicationStatus;
-import com.collegeadmission.model.UserDetails;
+
 
 public class ApplicationStatusImpl {
 
 	public void applicationStatus(ApplicationStatus appStatus) throws ClassNotFoundException, SQLException {
 
-		Connection con = ConnectionUtil.getDBConnect();
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+		
+		 con = ConnectionUtil.getDBConnect();
 		String query = " insert into application_status(user_id,application_id,course_id,payment_status,application_status)values(?,?,?,?,?)";
 
-		PreparedStatement stmt = con.prepareStatement(query);
-		// ApplicationDaoImpl appDao=new ApplicationDaoImpl();
-		stmt.setInt(1, appStatus.getUserId());
-		//System.out.println(appStatus.getUserId());
-		// int appID = appDao.findAppId(appStatus.getUserId());
+		 pstmt = con.prepareStatement(query);
+		
+		pstmt.setInt(1, appStatus.getUserId());
+		
+		pstmt.setInt(2, appStatus.getApplicationId());
+		
+		pstmt.setInt(3, appStatus.getCourseId());
+		
+		pstmt.setString(4, appStatus.getPaymentStatus());
+		
+		pstmt.setString(5, appStatus.getApplicationStatus());
 
-		stmt.setInt(2, appStatus.getApplicationId());
-		//System.out.println(appStatus.getApplicationId());
-		stmt.setInt(3, appStatus.getCourseId());
-		//System.out.println(appStatus.getCourseId());
-		stmt.setString(4, appStatus.getPaymentStatus());
-		stmt.setString(5, appStatus.getApplicationStatus());
+		pstmt.executeUpdate();
+		}
 
-		stmt.executeUpdate();
-		System.out.println("Insert Applicant Status Successfully");
+		catch (Exception e) {
+			e.getMessage();
+		} finally {
+			ConnectionUtil.close(pstmt, con);
+		}
+		
 	}
 
 	public void updateApplicationStatus(ApplicationStatus as1) throws ClassNotFoundException, SQLException {
 
-		Connection con = ConnectionUtil.getDBConnect();
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+		 con = ConnectionUtil.getDBConnect();
 
 		String update = "update application_status set payment_status=?, application_status=? where status_id=?";
 		System.out.println(update);
-		PreparedStatement stmt = con.prepareStatement(update);
+		pstmt = con.prepareStatement(update);
 
-		stmt.setString(1, as1.getPaymentStatus());
-		stmt.setString(2, as1.getApplicationStatus());
-		stmt.setInt(3, as1.getStatusId());
+		pstmt.setString(1, as1.getPaymentStatus());
+		pstmt.setString(2, as1.getApplicationStatus());
+		pstmt.setInt(3, as1.getStatusId());
+	}
 
-		System.out.println(stmt.executeUpdate() + " is updated !!");
+	catch (Exception e) {
+		e.getMessage();
+	} finally {
+		ConnectionUtil.close(pstmt, con);
+	}
+
+		System.out.println(pstmt.executeUpdate() + " is updated !!");
 
 	}
 
@@ -56,10 +78,18 @@ public class ApplicationStatusImpl {
 		String query = "select * from application_status";
 		ApplicationStatus appStatus;
 		List<ApplicationStatus> show = new ArrayList<ApplicationStatus>();
-		Connection con = ConnectionUtil.getDBConnect();
-		PreparedStatement ps = con.prepareStatement(query);
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+		
+		
+		 con = ConnectionUtil.getDBConnect();
+		 pstmt = con.prepareStatement(query);
 
-		ResultSet rs = ps.executeQuery();
+		 rs = pstmt.executeQuery();
 
 		while (rs.next()) {
 			appStatus = new ApplicationStatus(rs.getInt(1), rs.getInt(2), rs.getInt(3), rs.getInt(4), rs.getString(5),
@@ -68,6 +98,17 @@ public class ApplicationStatusImpl {
 			show.add(appStatus);
 
 		}
+		
+		
+	 } catch (ClassNotFoundException e) {
+		
+		e.printStackTrace();
+	} catch (Exception e) {
+		
+		e.printStackTrace();
+	} finally {
+		ConnectionUtil.close(rs,pstmt, con);
+	}
 
 		return show;
 	}
@@ -77,11 +118,18 @@ public class ApplicationStatusImpl {
 		String query = "select * from application_status where user_id=?";
 		ApplicationStatus appStatus;
 		List<ApplicationStatus> show = new ArrayList<ApplicationStatus>();
+		
+		Connection con = null;
+		
+		PreparedStatement pstmt = null;
+		
+		ResultSet rs = null;
+		
 		try {
-			Connection con = ConnectionUtil.getDBConnect();
-			PreparedStatement ps = con.prepareStatement(query);
-			ps.setInt(1, userId);
-			ResultSet rs = ps.executeQuery();
+			 con = ConnectionUtil.getDBConnect();
+			pstmt = con.prepareStatement(query);
+			pstmt.setInt(1, userId);
+			rs = pstmt.executeQuery();
 
 			while (rs.next()) {
 				appStatus = new ApplicationStatus(rs.getInt(2), rs.getInt(3), rs.getInt(4), rs.getString(5),
@@ -92,6 +140,8 @@ public class ApplicationStatusImpl {
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
+		}finally {
+			ConnectionUtil.close(rs,pstmt, con);
 		}
 		return show;
 
